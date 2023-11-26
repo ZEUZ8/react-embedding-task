c668bf21583917e94cc983d71da249024b31e08a
sinan <ptsinan8590@gmail.com>
2023-11-26 12:57:03 +0530

    Initiated React frontend task: Set up a simple HTML page using React CDN and Babel CDN. Completed the UI section with editable columns in either table format. Ready to implement the next steps for functionalities.

---
+++ nav.js
@@ -0,0 +1,21 @@
+const Nav = ()=>{
+    return(
+        <nav className="bg-white border-gray-200 dark:bg-gray-900 rounded-md" >
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
+        <a
+          href="https://redux.js.org/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            DYNAMIC TABLE
+          </span>
+        </a>
+      </div>
+    </nav>
+    )
+}
+
+
+// Render the component to the 'root' div
+const rootElement = document.getElementById('nav');
+ReactDOM.render(<Nav />, rootElement);
